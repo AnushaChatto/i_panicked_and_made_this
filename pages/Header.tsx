462470938dbfca35750bb90a15051b826c36724e
AnushaChatto <jhumpi.1997@gmail.com>
2025-01-12 00:03:06 +0100

    addition:corner button

--- pages/Header.tsx
+++ pages/Header.tsx
@@ -1,15 +1,16 @@
-
-import React from 'react';
+import React from "react";
 import styles from "../styles/Home.module.scss";
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-        <div className={styles.dropdown}>
-          <button className={styles.dropbtn}>Browse</button>
-          <div className={styles.dropdown_content}>
-            
-          </div>
-        </div>
+      <div className={`${styles.dropdown} ${styles.quarter_button}`}>
+        <button
+          className={`${styles.dropbtn} ${styles.quarter} ${styles.top_left_corner_button}`}
+        >
+          <span>Browse</span>
+        </button>
+        <div className={styles.dropdown_content}></div>
+      </div>
     </header>
   );
 };
